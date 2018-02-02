4c5be80cadc182f077eb94f202c1b8b07a869f98
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2018-02-02 19:52:52 +0100

    Mostrar las listas en un TableView

---
+++ ios/ListaCompra/ListaCompra/Lista.swift
@@ -0,0 +1,13 @@
+//
+//  Lista.swift
+//  ListaCompra
+//
+//  Created by Ion Jaureguialzo Sarasola on 2/2/18.
+//  Copyright © 2018 Ion Jaureguialzo Sarasola. All rights reserved.
+//
+
+import Foundation
+
+struct Lista {
+    var titulo: String
+}
