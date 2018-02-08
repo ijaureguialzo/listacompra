46ba466b56ab21cfe147ce0b9a2324aa9003b14d
Ion Jaureguialzo Sarasola <ion@jaureguialzo.com>
2018-02-08 15:46:53 +0100

    Creando un formulario con Eureka

---
+++ ios/ListaCompra/ListaCompra/NuevaListaViewController.swift
@@ -0,0 +1,42 @@
+//
+//  NuevaListaViewController.swift
+//  ListaCompra
+//
+//  Created by Ion Jaureguialzo Sarasola on 8/2/18.
+//  Copyright © 2018 Ion Jaureguialzo Sarasola. All rights reserved.
+//
+
+import UIKit
+
+import Eureka
+
+class NuevaListaViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        form +++ Section("Datos de la lista")
+        <<< TextRow() { row in
+            row.title = "Título"
+            row.placeholder = "Supermercado"
+        }
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
